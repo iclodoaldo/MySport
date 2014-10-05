b4b4d77bfd6183017a28726b359e82b9c771b092
iclodoaldo <iclodoaldo@ig.com.br>
2014-10-04 21:06:41 -0300

    Clodoaldo

---
+++ src/Atleta.java
@@ -0,0 +1,91 @@
+
+import java.util.Date;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Clodoaldo
+ */
+public class Atleta {
+    private String email;
+    private String senha;
+    private String nome;
+    private Date dataNas;
+    private double peso;
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * @return the senha
+     */
+    public String getSenha() {
+        return senha;
+    }
+
+    /**
+     * @param senha the senha to set
+     */
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the dataNas
+     */
+    public Date getDataNas() {
+        return dataNas;
+    }
+
+    /**
+     * @param dataNas the dataNas to set
+     */
+    public void setDataNas(Date dataNas) {
+        this.dataNas = dataNas;
+    }
+
+    /**
+     * @return the peso
+     */
+    public double getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+    
+    
+}
---
+++ src/MySport.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+/**
+ *
+ * @author Clodoaldo
+ */
+public class MySport {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+                
+    }
+
+    public boolean Login(){
+    //
+    return true;}
+    public void Logout(){
+        //
+    }
+    public void IniciarTreino(){
+        //
+    }
+    public void FinalizarTreino(){
+        //
+    }
+    public void SalvarTreino(){
+        //
+    }
+    
+    
+    
+}
+
+
